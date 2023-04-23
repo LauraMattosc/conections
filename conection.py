6e0a95a348d9b425a6cb7fbc099198f064e9386c
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-04-23 19:07:47 -0300

    Update conection.py

--- conection.py
+++ conection.py
@@ -83,71 +83,71 @@ def main():
                        fillcolor=colors[themes
 
    # Adicionar nós (pessoas) ao grafo de rede
-for i, person in enumerate(fake_data):
-    graph.add_node(i, label=person[0], theme=person[1], shape='circle', style='filled',
-                   fillcolor=colors[themes.index(person[1])], fontcolor='white', fontsize=10)
+    for i, person in enumerate(fake_data):
+        graph.add_node(i, label=person[0], theme=person[1], shape='circle', style='filled',
+                       fillcolor=colors[themes.index(person[1])], fontcolor='white', fontsize=10)
 
-# Adicionar conexões ao grafo de rede
-for i, person in enumerate(fake_data):
-    recommendations = get_recommendations(i, encoded_data)
-    for rec in recommendations:
-        graph.add_edge(i, rec)
-
-# Definir configurações do grafo de rede
-pos = nx.spring_layout(graph, seed=42)
-edge_x = []
-edge_y = []
-for edge in graph.edges():
-    x0, y0 = pos[edge[0]]
-    x1, y1 = pos[edge[1]]
-    edge_x.append(x0)
-    edge_x.append(x1)
-    edge_x.append(None)
-    edge_y.append(y0)
-    edge_y.append(y1)
-    edge_y.append(None)
-
-node_x = []
-node_y = []
-for node in graph.nodes():
-    x, y = pos[node]
-    node_x.append(x)
-    node_y.append(y)
-
-node_trace = go.Scatter(x=node_x, y=node_y, mode='markers', text=[person[0] for person in fake_data],
-                        marker=dict(color=[colors[themes.index(person[1])] for person in fake_data], size=10))
-
-edge_trace = go.Scatter(x=edge_x, y=edge_y, mode='lines', line=dict(width=0.5, color='gray'), hoverinfo='none')
-
-fig = go.Figure(data=[edge_trace, node_trace],
-                layout=go.Layout(title='Recomendações de pessoas por similaridade temática', showlegend=False,
-                                 hovermode='closest', margin=dict(b=20, l=5, r=5, t=40),
-                                 xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-                                 yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
-
-# Exibir grafo de rede
-st.plotly_chart(fig, use_container_width=True)
-
-# Obter as 3 pessoas mais parecidas com a primeira pessoa do conjunto de dados
-recommendations = get_recommendations(0, encoded_data)[:3]
-
-st.subheader('Pessoas mais parecidas')
-for rec in recommendations:
-    st.write(fake_data[rec][0])
-
-# Obter as 3 conexões com mais potencial
-potential_connections = []
-for i, person in enumerate(fake_data):
-    if i not in recommendations:
+    # Adicionar conexões ao grafo de rede
+    for i, person in enumerate(fake_data):
+        recommendations = get_recommendations(i, encoded_data)
         for rec in recommendations:
-            if rec in get_recommendations(i, encoded_data):
-                potential_connections.append((i, rec))
+            graph.add_edge(i, rec)
 
-sorted_potential_connections = sorted(potential_connections, key=lambda x: x[1], reverse=True)[:3]
+    # Definir configurações do grafo de rede
+    pos = nx.spring_layout(graph, seed=42)
+    edge_x = []
+    edge_y = []
+    for edge in graph.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
 
-st.subheader('Conexões com mais potencial')
-for connection in sorted_potential_connections:
-    st.write(fake_data[connection[0]][0], 'e', fake_data[connection[1]][0])
-                                     
+    node_x = []
+    node_y = []
+    for node in graph.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(x=node_x, y=node_y, mode='markers', text=[person[0] for person in fake_data],
+                            marker=dict(color=[colors[themes.index(person[1])] for person in fake_data], size=10))
+
+    edge_trace = go.Scatter(x=edge_x, y=edge_y, mode='lines', line=dict(width=0.5, color='gray'), hoverinfo='none')
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(title='Recomendações de pessoas por similaridade temática', showlegend=False,
+                                     hovermode='closest', margin=dict(b=20, l=5, r=5, t=40),
+                                     xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                                     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)))
+
+    # Exibir grafo de rede
+    st.plotly_chart(fig, use_container_width=True)
+
+    # Obter as 3 pessoas mais parecidas com a primeira pessoa do conjunto de dados
+    recommendations = get_recommendations(0, encoded_data)[:3]
+
+    st.subheader('Pessoas mais parecidas')
+    for rec in recommendations:
+        st.write(fake_data[rec][0])
+
+    # Obter as 3 conexões com mais potencial
+    potential_connections = []
+    for i, person in enumerate(fake_data):
+        if i not in recommendations:
+            for rec in recommendations:
+                if rec in get_recommendations(i, encoded_data):
+                    potential_connections.append((i, rec))
+
+    sorted_potential_connections = sorted(potential_connections, key=lambda x: x[1], reverse=True)[:3]
+
+    st.subheader('Conexões com mais potencial')
+    for connection in sorted_potential_connections:
+        st.write(fake_data[connection[0]][0], 'e', fake_data[connection[1]][0])
+
                                       
                                         
