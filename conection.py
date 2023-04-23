4610ee00734ae799bc2d7cc2e71e3cb58e6fa571
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-04-23 18:44:16 -0300

    Update conection.py

--- conection.py
+++ conection.py
@@ -8,7 +8,8 @@ import plotly.graph_objs as go
 import networkx as nx
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
-from streamlit_agraph import st_agraph
+import numpy as np
+
 
 
 # Gerar dados falsos
@@ -52,7 +53,7 @@ st.title('Clusters de Pessoas por Temática')
 st.write('Este aplicativo gera dados falsos de pessoas com temas aleatórios e os agrupa em clusters usando k-means e os visualiza em um gráfico interativo.')
 
 # Definir o número de pontos de dados
-n_points = st.slider('Selecione o número de pontos de dados a serem gerados', min_value=100, max_value=700, step=100, value=100)
+n_points = st.slider('Selecione o número de pontos de dados a serem gerados', min_value=100, max_value=200, step=100, value=100)
 
 # Gerar dados falsos
 fake_data = create_fake_data(n_points)
@@ -69,34 +70,45 @@ reduced_data = reduce_dimensionality(encoded_data)
 # Separar pontos por cluster
 cluster_points = separate_points(reduced_data, clusters)
 
+# Criar um gráfico interativo com plotly
+fig = go.Figure()
 
-# Criar nós (pessoas) do gráfico de rede
-nodes = []
+colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'black', 'purple']
+
+for i, points in cluster_points.items():
+    fig.add_trace(go.Scatter(x=points[0], y=points[1], mode='markers',
+                             marker=dict(color=colors[i], size=8),
+                             text=[f"{fake_data[j][0]}<br>{themes[i]}" for j in range(len(fake_data)) if clusters[j] == i],
+                             name=themes[i]))
+
+# Exibir o gráfico interativo
+st.plotly_chart(fig, use_container_width=True)
+
+def get_recommendations(person_index, encoded_data):
+    dist_matrix = distance_matrix(encoded_data, encoded_data)
+    sorted_indices = np.argsort(dist_matrix[person_index])
+    similar_recommendations = sorted_indices[1:4]
+    different_recommendations = sorted_indices[-3:]
+    return np.concatenate((similar_recommendations, different_recommendations))
+
+# Criar um grafo vazio
+G = nx.Graph()
+
+# Adicionar nós (pessoas) ao grafo
 for i, person in enumerate(fake_data):
-    nodes.append((i, {'label': person[0], 'theme': person[1]}))
+    G.add_node(i, label=person[0], theme=person[1])
 
-# Criar conexões do gráfico de rede
-edges = []
+# Adicionar arestas (conexões) ao grafo
 for i, person in enumerate(fake_data):
     recommendations = get_recommendations(i, encoded_data)
     for rec in recommendations:
-        edges.append((i, rec))
+        G.add_edge(i, rec)
 
-# Criar o grafo de rede
-graph = st.agraph.graph(name='Recomendações de pessoas por similaridade temática')
+# Desenhar o grafo de rede
+fig, ax = plt.subplots(figsize=(12, 12))
+pos = nx.spring_layout(G, seed=42, iterations=50, cooling=0.95)
+nx.draw(G, pos, node_color=[colors[themes.index(G.nodes[node]["theme"])] for node in G], with_labels=False, ax=ax)
+nx.draw_networkx_labels(G, pos, labels={node: G.nodes[node]["label"] for node in G}, font_size=8, ax=ax)
 
-# Adicionar nós (pessoas) ao grafo de rede
-for node in nodes:
-    graph.add_node(name=str(node[0]), label=node[1]['label'], shape='circle', style='filled',
-                   fillcolor=colors[themes.index(node[1]['theme'])], fontcolor='white', fontsize=10)
-
-# Adicionar conexões ao grafo de rede
-for edge in edges:
-    graph.add_edge(str(edge[0]), str(edge[1]))
-
-# Definir configurações do grafo de rede
-graph.node_attr.update(fontname='Helvetica', fontcolor='black')
-graph.edge_attr.update(color='gray', arrowsize=0.8)
-
-# Exibir grafo de rede
-st.graphviz_chart(graph)
+# Exibir gráfico
+st.pyplot(fig)
