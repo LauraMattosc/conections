d4d4da74e133e917fbb28096003f578958534abf
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-04-23 18:02:03 -0300

    Update conection.py

--- conection.py
+++ conection.py
@@ -14,13 +14,10 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Gerar dados falsos
-fake = Faker('pt_BR')
-themes = ['Educação', 'Saúde', 'Meio Ambiente', 'Finanças', 'Tecnologia', 'Direito', 'Marketing', 'Logística']
-
-# Definir as cores para os temas
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f']
-
+@st.cache
 def create_fake_data(n):
+    fake = Faker('pt_BR')
+    themes = ['Educação', 'Saúde', 'Meio Ambiente', 'Finanças', 'Tecnologia', 'Direito', 'Marketing', 'Logística']
     data = []
     for _ in range(n):
         name = fake.name()
